0b958d4275613151654ace6cb49dfba5c5cefbfe
Karl Behrens <karlbehrensg@gmail.com>
2020-11-22 19:19:41 -0300

    Managing modules and their contents

--- courses/views.py
+++ courses/views.py
@@ -1,13 +1,15 @@
 from django.urls import reverse_lazy
-from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, \
+    DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, \
+    PermissionRequiredMixin
 from django.forms.models import modelform_factory
 from django.apps import apps
-from .forms import ModuleFormSet
 from .models import Course, Module, Content
+from .forms import ModuleFormSet
 
 
 class OwnerMixin(object):
@@ -22,7 +24,9 @@ class OwnerEditMixin(object):
         return super().form_valid(form)
 
 
-class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
+class OwnerCourseMixin(OwnerMixin,
+                       LoginRequiredMixin,
+                       PermissionRequiredMixin):
     model = Course
     fields = ['subject', 'title', 'slug', 'overview']
     success_url = reverse_lazy('manage_course_list')
@@ -55,22 +59,27 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
     course = None
 
     def get_formset(self, data=None):
-        return ModuleFormSet(instance=self.course, data=data)
+        return ModuleFormSet(instance=self.course,
+                             data=data)
 
     def dispatch(self, request, pk):
-        self.course = get_object_or_404(Course, id=pk, owner=request.user)
+        self.course = get_object_or_404(Course,
+                                        id=pk,
+                                        owner=request.user)
         return super().dispatch(request, pk)
 
     def get(self, request, *args, **kwargs):
         formset = self.get_formset()
-        return self.render_to_response({'course': self.course, 'formset': formset})
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
 
     def post(self, request, *args, **kwargs):
         formset = self.get_formset(data=request.POST)
         if formset.is_valid():
             formset.save()
             return redirect('manage_course_list')
-        return self.render_to_response({'course': self.course, 'formset': formset})
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
 
 
 class ContentCreateUpdateView(TemplateResponseMixin, View):
@@ -81,11 +90,15 @@ class ContentCreateUpdateView(TemplateResponseMixin, View):
 
     def get_model(self, model_name):
         if model_name in ['text', 'video', 'image', 'file']:
-            return apps.get_model(app_label='courses', model_name=model_name)
+            return apps.get_model(app_label='courses',
+                                  model_name=model_name)
         return None
 
     def get_form(self, model, *args, **kwargs):
-        Form = modelform_factory(model, exclude=['owner', 'order', 'created', 'updated'])
+        Form = modelform_factory(model, exclude=['owner',
+                                                 'order',
+                                                 'created',
+                                                 'updated'])
         return Form(*args, **kwargs)
 
     def dispatch(self, request, module_id, model_name, id=None):
@@ -132,3 +145,14 @@ class ContentDeleteView(View):
         content.item.delete()
         content.delete()
         return redirect('module_content_list', module.id)
+
+
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/content_list.html'
+
+    def get(self, request, module_id):
+        module = get_object_or_404(Module,
+                                   id=module_id,
+                                   course__owner=request.user)
+
+        return self.render_to_response({'module': module})
