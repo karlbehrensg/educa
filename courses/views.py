35e6d9cdd5b45c100d24feaf43eb36cd6d9fa18e
Karl Behrens <karlbehrensg@gmail.com>
2020-11-26 01:30:54 -0300

    Adding Memcached

--- courses/views.py
+++ courses/views.py
@@ -9,6 +9,7 @@ from django.views.generic.detail import DetailView
 from django.forms.models import modelform_factory
 from django.apps import apps
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+from django.core.cache import cache
 from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
 from students.forms import CourseEnrollForm
@@ -179,7 +180,10 @@ class CourseListView(TemplateResponseMixin, View):
     template_name = 'courses/course/list.html'
 
     def get(self, request, subject=None):
-        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        subjects = cache.get('all_subjects')
+        if not subjects:
+            subjects = Subject.objects.annotate(total_courses=Count('courses'))
+            cache.set('all_subjects', subjects)
         courses = Course.objects.annotate(total_modules=Count('modules'))
         if subject:
             subject = get_object_or_404(Subject, slug=subject)
